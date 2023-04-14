923cfe0b334f2107f56cd17ac57834a771539da2
Alejandro Ramirez <jano@jano.dev>
2023-04-14 22:00:33 +0200

    Update swiftai.rb

--- Formula/swiftai.rb
+++ Formula/swiftai.rb
@@ -4,7 +4,7 @@ class Swiftai < Formula
   url "https://github.com/janodevorg/SwiftAI/releases/download/1.0.4/1.0.4.tar.gz"
   sha256 "93d1530ec9120e89a04bf6204034f498061695acebb7767a897385b7ffa295da"
   license "MIT"
-  version "1.0.4"
+  version "0.0.1"
   def install
     bin.install "swiftai"
   end
