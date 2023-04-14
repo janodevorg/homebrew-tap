5eaca0dbfb308ddd7bc1f223f8da4b44211e931f
Alejandro Ramirez <jano@jano.dev>
2023-04-14 16:37:32 +0200

    no message

--- Formula/swiftai-cli.rb
+++ Formula/swiftai-cli.rb
@@ -4,7 +4,7 @@ class SwiftAICli < Formula
   url "https://github.com/janodevorg/SwiftAI/archive/refs/tags/1.0.0.tar.gz"
   sha256 "6ba2c8d26b02469865817f1e39c3396d8f71970251845207286801135e798aff"
   license "MIT"
-  version "1.0.0"
+  version "0.0.1"
   def install
     bin.install "swiftai"
   end
