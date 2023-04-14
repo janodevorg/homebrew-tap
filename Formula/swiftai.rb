71c0aafeb228cf5c025fc7f7327734e0eb979aca
Alejandro Ramirez <jano@jano.dev>
2023-04-14 22:06:13 +0200

    Update swiftai.rb

--- Formula/swiftai.rb
+++ Formula/swiftai.rb
@@ -1,7 +1,7 @@
 class Swiftai < Formula
   desc "A Swift CLI implementation of the OpenAI API"
   homepage ""
-  url "https://github.com/janodevorg/SwiftAI/releases/download/1.0.4/1.0.4.tar.gz"
+  url "https://github.com/janodevorg/SwiftAI/releases/download/0.0.1/0.0.1.tar.gz"
   sha256 "93d1530ec9120e89a04bf6204034f498061695acebb7767a897385b7ffa295da"
   license "MIT"
   version "0.0.1"
