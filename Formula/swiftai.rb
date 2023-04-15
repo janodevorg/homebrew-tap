1b736002fed919926013d9a911bdda2a2e63d8eb
Alejandro Ramirez <jano@jano.dev>
2023-04-15 17:31:35 +0200

    order the elements according to brew audit

--- Formula/swiftai.rb
+++ Formula/swiftai.rb
@@ -1,10 +1,10 @@
 class Swiftai < Formula
-  desc "A Swift CLI implementation of the OpenAI API"
-  homepage ""
-  url "https://github.com/janodevorg/SwiftAI/releases/download/1.0.0/1.0.0.tar.gz"
-  sha256 "896c733f1ff032c6aea1d3f0fd5c82a852c741e9e43194c7635112888fdb9c46"
+  desc "Swift CLI implementation of the OpenAI API"
+  homepage "https://janodevorg.github.io/SwiftAI/documentation/swiftai/"
+  url "https://github.com/janodevorg/SwiftAI/releases/download/1.0.4/1.0.4.tar.gz"
+  version "1.0.4"
+  sha256 "93d1530ec9120e89a04bf6204034f498061695acebb7767a897385b7ffa295da"
   license "MIT"
-  version "1.0.0"
   def install
     bin.install "swiftai"
   end
