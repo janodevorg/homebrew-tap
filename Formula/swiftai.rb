6108c89e260dacc7317aca6779c8aadbd7400db2
Alejandro Ramirez <jano@jano.dev>
2023-04-15 17:37:02 +0200

    swiftai 1.0.0
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/swiftai.rb
+++ Formula/swiftai.rb
@@ -1,9 +1,9 @@
 class Swiftai < Formula
   desc "Swift CLI implementation of the OpenAI API"
   homepage "https://janodevorg.github.io/SwiftAI/documentation/swiftai/"
-  url "https://github.com/janodevorg/SwiftAI/releases/download/0.0.1/0.0.1.tar.gz"
-  version "0.0.1"
-  sha256 "93d1530ec9120e89a04bf6204034f498061695acebb7767a897385b7ffa295da"
+  url "https://github.com/janodevorg/SwiftAI/releases/download/1.0.0/1.0.0.tar.gz"
+  version "1.0.0"
+  sha256 "65efd4cfd08b4ac53c34f9249dc50c59854c49eedd4c48ed380b46d467ad899f"
   license "MIT"
   def install
     bin.install "swiftai"
